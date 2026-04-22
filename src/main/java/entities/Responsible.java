818e78a390a82d5112e01c0dbed0247610c62d4f
Jefferson Ferreira <jeffersonmatheusu@gmail.com>
2026-04-22 07:39:44 -0300

    :sparkles: feat: Mapeando as entidades de tarefa e responsavel

--- src/main/java/entities/Responsible.java
+++ src/main/java/entities/Responsible.java
@@ -1,5 +1,54 @@
 package entities;
 
-public class Responsible {
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_responsibles")
+public class Responsible implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String name;
+
+	public Responsible(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Responsible() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 }
+
--- src/main/java/enums/Priority.java
+++ src/main/java/enums/Priority.java
@@ -1,5 +1,15 @@
 package enums;
 
 public enum Priority {
+	BAIXA("BAIXA"), MEDIA("MÉDIA"), ALTA("ALTA");
 
+	private String description;
+
+	Priority(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return description;
+	}
 }
